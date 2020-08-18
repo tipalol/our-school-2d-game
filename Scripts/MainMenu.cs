12307f3390b5bf875a8e9e7cf71ab8087dea8cc7
Sorokin Dmitrii <tipalol2@gmail.com>
2020-08-18 20:22:26 +0500

    Add files via upload

---
+++ Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject StorePanel;
+
+    public void Exit() 
+    {
+        Application.Quit(0);
+    }
+
+    public void Play() 
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void Settings() 
+    {
+        Debug.Log("Зашли в настройки");
+    }
+
+    public void Store() 
+    {
+        StorePanel.SetActive(true);
+    }
+
+    public void CloseStore() 
+    {
+        StorePanel.SetActive(false);
+    }
+}
